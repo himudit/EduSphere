f644b2d6a4b308328b44830df88f29de3b7c35a7
himudit <gargmudit662@gmail.com>
2025-06-16 12:35:30 +0530

    no chnage

--- Frontend/Edusphere/src/components/Home.tsx
+++ Frontend/Edusphere/src/components/Home.tsx
@@ -13,16 +13,16 @@ const Home: React.FC = () => {
       <div className="flex flex-col items-center justify-center min-h-screen text-white px-6 overflow-hidden">
         {/* Main Heading */}
 
-        {/* <h1 className="text-7xl md:text-6xl mb-4 text-center font-bold min-h-[100px]">
+        <h1 className="text-7xl md:text-6xl mb-4 text-center font-bold min-h-[100px]">
           Welcome to
-        </h1> */}
+        </h1>
 
-        <h1
+        {/* <h1
           className="text-5xl md:text-6xl font-bold text-center leading-snug"
           style={{ fontFamily: `'Segoe UI', Roboto, Helvetica, Arial, sans-serif` }}
         >
           Welcome to
-        </h1>
+        </h1> */}
         
 
         {/* Highlighted Gradient Text */}
